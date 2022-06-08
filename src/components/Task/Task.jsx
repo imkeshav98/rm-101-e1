fbf3d2d64ed947393b92213f682c223cbfd6bede
imkeshav98 <imkeshav98@gmail.com>
2022-06-08 21:58:44 +0530

    fix

--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -1,16 +1,48 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { TodoContext } from "../../context/todoContext";
 import Counter from "../Counter/Counter";
 import styles from "./task.module.css";
 
 const Task = () => {
   const { todo, handleChange } = useContext(TodoContext);
+
+  function handleClick(id) {
+    let task = todo.filter((task) => task.id === id)[0];
+    handleChange(
+      todo.map((task) =>
+        task.id === id ? { ...task, done: !task.done } : task
+      )
+    );
+  }
+
   return (
     <>
       {todo.map((task) => (
-        <li data-testid="task" className={styles.task}>
-          <input type="checkbox" data-testid="task-checkbox" />
-          <div data-testid="task-text">{task.text}</div>
+        <li data-testid="task" className={styles.task} key={task.id}>
+          {task.done ? (
+            <input
+              type="checkbox"
+              data-testid="task-checkbox"
+              onClick={() => {
+                handleClick(task.id);
+              }}
+              defaultChecked
+            />
+          ) : (
+            <input
+              type="checkbox"
+              data-testid="task-checkbox"
+              onClick={() => {
+                handleClick(task.id);
+              }}
+            />
+          )}
+          <div
+            data-testid="task-text"
+            className={task.done ? "notchecked" : "checked"}
+          >
+            {task.text}
+          </div>
           <Counter countvalue={task.count} />
           <button data-testid="task-remove-button">x</button>
         </li>
